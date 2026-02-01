9738835dd9f377a98251315c1d639bf1911861f0
Nick Welch <nickwelch22@gmail.com>
2026-02-01 12:19:32 -0500

    Add Needham page with 3 ADU permits from quarterly reports

--- src/components/TownNav.tsx
+++ src/components/TownNav.tsx
@@ -9,6 +9,7 @@ const towns = [
   { name: 'Andover', href: '/andover' },
     { name: 'Falmouth', href: '/falmouth' },
   { name: 'Newton', href: '/newton' },
+  { name: 'Needham', href: '/needham' },
 ]
 
 export default function TownNav({ current }: { current: string }) {

5687a4470b75f08d816bfc07312b34d5fc67a23d
Nick Welch <nickwelch22@gmail.com>
2026-02-10 20:54:17 -0500

    remove old BottomNav from layout

--- src/components/ClientProviders.tsx
+++ src/components/ClientProviders.tsx
@@ -1,16 +1,12 @@
 'use client'
 
 import { TownProvider } from '@/contexts/TownContext'
-import BottomNav from '@/components/BottomNav'
 import { ReactNode } from 'react'
 
 export default function ClientProviders({ children }: { children: ReactNode }) {
   return (
     <TownProvider>
-      <div className="pb-16 md:pb-0">
-        {children}
-      </div>
-      <BottomNav />
+      {children}
     </TownProvider>
   )
 }
